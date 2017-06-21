e79c25cc62e1443d9490b3bc8e861defeb143a4e
Mao <maot.tm@gmail.com>
2017-06-21 21:58:59 +0100

    Build template function added

---
+++ build-page.js
@@ -0,0 +1,105 @@
+function(tags, url) {
+	return `
+	<!DOCTYPE html>
+	<html slick-uniqueid="3"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	    <link rel="stylesheet" type="text/css" href="https://comprindeo.intelligentvoice.com/smarttranscript.css">
+	    <script>
+	        function scrollToView() {
+	            var a = document.getElementById("doc-scroller");
+	            var d = a.getElementsByClassName('highlight');
+	            if (d.length) {
+	                d = d[0];
+	                var scrollHeight = a.offsetHeight;
+	                var scrollOffset = a.scrollTop;
+	                var targetOffset = d.offsetTop;
+	                // Scroll into viewport only if out of bounds.
+	                if ((targetOffset > (scrollHeight + scrollOffset)) || (targetOffset < scrollOffset)) {
+	                    a.scrollTop = targetOffset - 20;
+	                }
+	            }
+	        }
+	        function getClosestValues(a, x) {
+	            var lo = -1, hi = a.length;
+	            while (hi - lo > 1) {
+	                var mid = Math.round((lo + hi) / 2);
+	                if (a[mid] <= x) {
+	                    lo = mid;
+	                } else {
+	                    hi = mid;
+	                }
+	            }
+	            if (a[lo] === x)
+	                hi = lo;
+	            return [a[lo], a[hi]];
+	        }
+	        function seek2(pos, offset) {
+	            if (!isNaN(offset)) {
+	                pos += offset;
+	                if (pos < 0) {
+	                    pos = 0;
+	                }
+	            }
+	            var mediaElement = document.getElementById('media_player');
+	            mediaElement.currentTime = pos / 1000;
+	            scrollToView();
+	        }
+	    </script>
+	</head>
+	<body>
+	<div class="ui-widget ui-widget-content" id="document-div">
+	    <table>
+	        <tbody><tr>
+	            <td>
+	                <div id="topics">
+	                    <h2><em>JumpTo</em> Topics</h2><br>
+	                    <div id="topics-container">
+	                    	${tags}
+	                    </div>
+	                </div>
+	            </td>
+	            <td id="table_1">
+	                <div id="doc-main">
+	                   
+	                    <video id="media_player" controls=""><source src="${url}"></video>
+	                    <!-- <div id="position"></div> -->
+	                    <div style="position:relative;" id="doc-scroller"></div>
+	                </div>
+	            </td>
+	        </tr>
+	    </tbody></table>
+	</div>
+	<script>
+	    var media_player = document.getElementById('media_player'),
+	        current_links = [],
+	        linkers = [];
+	    document.addEventListener('DOMContentLoaded', function () {
+	        media_player.play(); // Auto-buffer.
+	        media_player.pause();
+	        media_player.addEventListener("timeupdate", function () {
+	            for (var i = 0; i < current_links.length; i++) {
+	                if (!isNaN(current_links[i])) {
+	                    document.getElementById("position_" + current_links[i]).className = "";
+	                }
+	            }
+	            var g = getClosestValues(linkers, media_player.currentTime * 1000);
+	            current_links = g;
+	            var a = document.getElementById("doc-scroller");
+	            for (var i = 0; i < g.length; i++) {
+	                if (!isNaN(g[i])) {
+	                    var d = document.getElementById("position_" + g[i]);
+	                    if (d != null) {
+	                        d.className = "highlight";
+	                        scrollToView();
+	                    }
+	                }
+	            }
+	        });
+	    });
+	    // @body
+	    var body = '',
+	        spkr = 0;
+	    
+	    </script> 
+	</body></html>
+	`
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var path = require('path')
 var fs = require('fs')
 
 var p = require('xml2js').parseString;
+var _html = [] // String that collects 'HTML' elements 
 
 fs.readFile(path.resolve(__dirname, '30000001.xml'), function(err, data) {
          
@@ -15,7 +16,37 @@ fs.readFile(path.resolve(__dirname, '30000001.xml'), function(err, data) {
 
 
             fs.readFile(path.resolve(__dirname, 'json','30000001.json'), 'utf8', function(err, data) {
-              JSON.parse(data).Item.tags.map(t => console.log(t))
+              
+               
+
+              JSON.parse(data).Item.tags.map(tags => {
+    
+                tags.tags.map(tagObj => {
+
+                    let { tag, position } = tagObj
+                    
+                    let tagName = tag[0] //tag[0] === <tag_name>
+                    
+                    if(position) {
+                        position.map((p, i) => { 
+                            //position is array of positions of a tag
+                            
+                            let timestamp = p.timestamp[0]
+                            
+                            if(i === 0) { //1st instance of tag found in transcript
+                                
+                                _html.push(`<a class="topics" href="javascript:seek2(${timestamp}, -5000);">${tagName}</a>`)
+                            } else if (i > 0 && i < position.length - 1) { //All subsequent instances of tag found in transcript
+                                _html.push(`(<a class="topics" href="javascript:seek2(${timestamp}, -5000);">${i+1}</a>)`)
+                            } else {
+                                _html.push(`(<a class="topics" href="javascript:seek2(${timestamp}, -5000);">${i+1}</a>)<br>`)
+                            }
+                        })
+                    }               
+                })
+                
+              })
+              console.log(_html.toString())
             })
         })
     });  
